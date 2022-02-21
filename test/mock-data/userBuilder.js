d23123a9c5b1043af8aade38a00c651b99a78fcc
Stephen Welbourn <welbourn.stephen@gmail.com>
2022-02-21 04:22:15 -0600

    ensure userBuilder email is all lowercase

--- test/mock-data/userBuilder.js
+++ test/mock-data/userBuilder.js
@@ -3,7 +3,7 @@ const { build, fake } = require('@jackfranklin/test-data-bot');
 exports.userBuilder = build('User', {
   fields: {
     name: fake((f) => f.name.findName()),
-    email: fake((f) => f.internet.email()),
+    email: fake((f) => f.internet.email().toLowerCase()),
     tagline: fake((f) => f.lorem.sentence()),
     bio: fake((f) => f.lorem.paragraph()),
     interests: fake((f) => f.lorem.sentence()),
