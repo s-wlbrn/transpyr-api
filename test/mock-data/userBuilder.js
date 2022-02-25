fb04a2d0f910f0be05a6c2d598e0aa584022c3a1
Stephen Welbourn <welbourn.stephen@gmail.com>
2022-02-25 05:06:32 -0600

    add mock user builder favorites and active fields

--- test/mock-data/userBuilder.js
+++ test/mock-data/userBuilder.js
@@ -1,4 +1,6 @@
-const { build, fake } = require('@jackfranklin/test-data-bot');
+const mongoose = require('mongoose');
+const { build, perBuild, fake } = require('@jackfranklin/test-data-bot');
+const { randomInteger } = require('../helpers/randomInteger');
 
 exports.userBuilder = build('User', {
   fields: {
@@ -8,8 +10,17 @@ exports.userBuilder = build('User', {
     bio: fake((f) => f.lorem.paragraph()),
     interests: fake((f) => f.lorem.sentence()),
     privateFavorites: fake((f) => f.datatype.boolean()),
-    role: 'user',
-    password: 'testpassword',
-    passwordConfirm: 'testpassword',
+    favorites: perBuild(() => {
+      const favorites = Array(randomInteger(1, 10))
+        .fill(null)
+        .map(() => {
+          return mongoose.Types.ObjectId().toString();
+        });
+      return favorites;
+    }),
+    role: perBuild(() => 'user'),
+    password: perBuild(() => 'testpassword'),
+    passwordConfirm: perBuild(() => 'testpassword'),
+    active: perBuild(() => true),
   },
 });
